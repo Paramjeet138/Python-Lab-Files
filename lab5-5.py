c0868e80146d1bb735e5c27fe25f822ddda03dc2
Paramjeet138 <24bee138@spt.pdpu.ac.in>
2025-04-24 19:27:50 +0530

    Add files via upload

---
+++ lab5-5.py
@@ -0,0 +1,11 @@
+print("Paramjeetsinh Jadeja 24BEE138")
+list =[]
+def upper(l):
+    l =["hello","bye","Python","how","hey"]
+    g=[]
+    for i in range(len(l)):
+        f=l[i].upper()
+        g.append(f)
+    print(g)
+       
+upper(list)
